2fe07b1cdb3afc860c037fd298052b32587cb669
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2024-09-04 20:18:26 +0700

    update modify

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,8 +1,11 @@
 // pages/api/config.js
+export const config = {
+  runtime: 'edge', // Cấu hình để sử dụng Edge Runtime
+};
+
 export default function handler(req, res) {
-    res.status(200).json({
-      botToken: process.env.NEXT_PUBLIC_BOT_TOKEN,
-      chatId: process.env.NEXT_PUBLIC_CHAT_ID
-    });
-  }
-  
+  res.status(200).json({
+    botToken: process.env.NEXT_PUBLIC_BOT_TOKEN,
+    chatId: process.env.NEXT_PUBLIC_CHAT_ID,
+  });
+}
